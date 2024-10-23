9e2446b4cb4803a5b7fd670fc476660c6f3e78dc
Maclaum06 <maclaum@gmail.com>
2024-10-22 21:56:44 -0500

    index.js

--- 2 entrega/js/index.js
+++ 2 entrega/js/index.js
@@ -1,39 +1,46 @@
+
 const PRODUCTOS_ARRAY = [
     {
         id: 1,
-        nombre: 'Camiseta hombre',
-        precio: 100
+        nombre: 'iphone 16',
+        precio: 1500,
+        marca: 'apple'
     },
     {
         id: 2,
-        nombre: 'Camiseta mujer',
-        precio: 120
+        nombre: 'Samsung S24',
+        precio: 1450,
+         marca: 'samsung'
     },
     {
         id: 3,
-        nombre: 'Pantalón hombre',
-        precio: 200
+        nombre: 'Iphone 13',
+        precio: 850,
+         marca: 'apple'
     },
     {
         id: 4,
-        nombre: 'Pantalón mujer',
-        precio: 220
+        nombre: 'samsung s23',
+        precio: 650,
+        marca: 'samsung'
     },
     {
         id: 5,
-        nombre: 'Cartera',
-        precio: 50
+        nombre: 'Iphone 16 pro',
+        precio: 1950,
+        marca: 'samsung'
     },
 ];
 
 let carrito = [];
 
-function mostrarProductos() {
-    const PRODUCTOS_SECTION = document.getElementById('productos'); // Corregido
-    PRODUCTOS_ARRAY.forEach(producto => {
-        const CARD_DIV = document.createElement('div'); // Corregido
+function mostrarProductos(FILTRADO_PRODUCTOS) {
+    const PRODUCTOS_SECTION = document.getElementById('productos'); 
+    PRODUCTOS_SECTION.innerHTML = '';
+    FILTRADO_PRODUCTOS.forEach(producto => {
+        const CARD_DIV = document.createElement('div'); 
         CARD_DIV.className = 'card-container';
-        CARD_DIV.innerHTML =  // Corregido
+        CARD_DIV.innerHTML = 
         `
         <h3>${producto.nombre}</h3>
         <p>${producto.precio}</p>
@@ -43,4 +50,18 @@ function mostrarProductos() {
     });
 }
 
-mostrarProductos();
+function filtrarProductos (marca) {
+    let FILTRADO_PRODUCTOS = marca
+    if(marca){
+        FILTRADO_PRODUCTOS = PRODUCTOS_ARRAY.filter(producto => producto.marca === marca)
+    }else{
+        FILTRADO_PRODUCTOS = PRODUCTOS_ARRAY
+    }
+    mostrarProductos(FILTRADO_PRODUCTOS)
+}
+
+mostrarProductos(PRODUCTOS_ARRAY);
+
+document.getElementById('todos').addEventListener('click', ()=>filtrarProductos(''));
+document.getElementById('apple').addEventListener('click', ()=>filtrarProductos('apple'));
+document.getElementById('samsung').addEventListener('click', ()=>filtrarProductos('samsung'));
